f83f3439ce257624c19dd719888a0d28695332c2
Alexd1717 <aduval@synergis.ca>
2022-03-25 13:16:55 -0400

    PPCM Terminer avec commentaire pour les deux fonctions et l'Affichage. Il reste juste une vérification

--- src/com/Calculator.java
+++ src/com/Calculator.java
@@ -6,38 +6,29 @@ public class Calculator {
     // Algorithme d'Euclide
     public int Calculer_plus_grand_diviseur(int nb1, int nb2)
     {
-        int r=0;
-        while(nb2!=0)
+        int result = 0;
+        while(nb2 != 0)// on verifie chaque nombre jusqu'a arriver à 0 et sortir de la boucle
         {
-            r=nb1%nb2;
-            nb1=nb2;
-            nb2=r;
+            result = nb1 % nb2; // encapsuler le resultat du modulo des deux nombres
+            nb1 = nb2; // Le diviseur est maintenant dans la variable nb1
+            nb2 = result;
         }
-        return nb1;
+        return nb1; // on retourne la valeur nb1
     }
-    
-    /*public int Calculer_plus_petit_commun_multiple(int nb1, int nb2)
+
+    // Definition 8
+    public int Calculer_plus_petit_commun_multiple(int nb1, int nb2)
     {
-        arraysNb1[i] = nb1;
-        arraysNb2[i] = nb2;
+        int divisor, modulo, result;
 
-        while(divisor != 0)
-        {
-            arraysNb1[i+1] =  nb1 * nb1;
-            arraysNb2[i+1] =  nb2 * nb2;
-
-            for(int y = 1; y <= arraysNb1.length; y++)
-            {
-                for(int x = 1; x <= arraysNb2.length; x++)
-                {
-                    if(arraysNb1[y] == arraysNb2[x])
-                    {
-                        divisor = arraysNb1[y];
-                    }
-                }
-            }
-            i++;
+        divisor = nb1 * nb2; // Le divisor encapsule la multiplication des deux nombres
+        modulo = nb1 % nb2; // Le modulo encapsule le modulo du nb2
+        while(modulo != 0){ // On va essayer de faire un modulo de chaque nombre jusqu'a ce que la valeur donne 0. Ensuite on sort de la boucle.
+            nb1 = nb2;
+            nb2 = modulo; // On entre la réponse dans le nb2
+            modulo = nb1 % nb2; // modulo va prendre la nouvelle valeur du modulo de nb2
         }
-        return divisor;
-    }*/
+        result = divisor / nb2; // result va encapsuler le resultat de la division entre le divisor et nb2
+        return result;
+    }
 }
--- src/com/Main.java
+++ src/com/Main.java
@@ -1,12 +1,22 @@
 // Alexy Duval Gabriel Deschenes
 package com;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
+        Scanner myObj = new Scanner(System.in);
 
-        System.out.println(calculator.Calculer_plus_grand_diviseur(2,4));
-        //System.out.println(calculator.Calculer_plus_petit_commun_multiple(6,8));
+        System.out.println("Saisir le premier nombre : ");
+        String number1 = myObj.nextLine();
+        int nb1 = Integer.parseInt(number1);
+
+        System.out.println("Saisir le deuxième nombre : ");
+        String number2 = myObj.nextLine();
+        int nb2 = Integer.parseInt(number2);
+
+        System.out.println("Le plus grand commun diviseur de "+ nb1 +" et "+ nb2 +" est : " +calculator.Calculer_plus_grand_diviseur(nb1,nb2));
+        System.out.println("Le plus petit commun multiple de "+ nb1 +" et "+ nb2 +" est : "+ calculator.Calculer_plus_petit_commun_multiple(nb1,nb2));
+
     }
 }
 
